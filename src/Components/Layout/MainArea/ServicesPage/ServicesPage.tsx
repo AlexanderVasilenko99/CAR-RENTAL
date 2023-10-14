c4cc25c9c6ee0126684bc35ef54e744e877ad5db
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-14 18:34:10 +0300

    added services with placeholders

--- src/Components/Layout/MainArea/ServicesPage/ServicesPage.tsx
+++ src/Components/Layout/MainArea/ServicesPage/ServicesPage.tsx
@@ -5,10 +5,26 @@ function ServicesPage(): JSX.Element {
     return (
         <div className="ServicesPage">
             <div className="image-container">
-                <img src={require("../../../../Assets/Images/road2.png")} />
-                <div className="image-container-content">
+                {/* <img src={require("../../../../Assets/Images/road2.png")} /> */}
+                <div>
                     <SingleService />
                 </div>
+                <div>
+                    <SingleService />
+                </div>
+                <div>
+                    <SingleService />
+                </div>
+                <div>
+                    <SingleService />
+                </div>
+                <div>
+                    <SingleService />
+                </div>
+                <div>
+                    <SingleService />
+                </div>
+
             </div>
 
         </div>
--- src/Components/Layout/MainArea/ServicesPage/SingleService/SingleService.tsx
+++ src/Components/Layout/MainArea/ServicesPage/SingleService/SingleService.tsx
@@ -1,9 +1,23 @@
+import { NavLink } from "react-router-dom";
 import "./SingleService.css";
 
 function SingleService(): JSX.Element {
     return (
         <div className="SingleService">
-			aaaa
+            <div className="background"></div>
+            <div className="content">
+                <div>
+
+                    <img src={require("../../../../../Assets/Images/raanana.jpg")} alt="" />
+                    <h3>Fuck bitches</h3>
+                    <p>Lorem ipsum dolor sitg elit. Aliquidm quam, architecto, molestias quas omnis magnam tempore perspiciatis corporis aspernatur.</p>
+                </div>
+
+                <div>
+                    <NavLink to={'#'}>Learn More</NavLink>
+                </div>
+
+            </div>
         </div>
     );
 }
