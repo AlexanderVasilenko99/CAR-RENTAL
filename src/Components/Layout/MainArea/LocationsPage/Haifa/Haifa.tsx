894f246cd72363aad84f647cc19cdb3637eb6b66
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-17 13:04:11 +0300

    added haifa and jerusalem

--- src/Components/Layout/MainArea/LocationsPage/Haifa/Haifa.tsx
+++ src/Components/Layout/MainArea/LocationsPage/Haifa/Haifa.tsx
@@ -1,11 +1,17 @@
-import Header from "../Header/Header";
+import FullCity from "../FullCity/FullCity";
 import "./Haifa.css";
 
 function Haifa(): JSX.Element {
     return (
         <div className="Haifa">
-            <Header />
-			Haifa
+            <FullCity city={"Haifa"} email='vasirental@hfa.co.il'
+                address="Yitshak Sadeh 10 st, Haifa." redirectPath="#" phoneNumber="" aboutLocation="The Haifa branch is
+                        located in Haifa's city center. Placed in a strategic location between Haifa's Shrine Of The Bab,
+                        Haifa's Museum of art, Haifa's old city, Haifa's zoo and the iconic Sderot Ben Gurion street and
+                        it's many delicious restaurants. The location a great starting point for exploring northern Israel.
+                        Make sure to visit all the beautiful sites of mount Carmel, both Galil provinces and, Acres and
+                        Tiberias!" reachOfficeInstructions="Our office is located at Yitshak Sadeh 10 st, Haifa. You can find
+                        us on the ground floor. There is plenty of parking on the street and in the area."/>
         </div>
     );
 }
--- src/Components/Layout/MainArea/LocationsPage/Jerusalem/Jerusalem.tsx
+++ src/Components/Layout/MainArea/LocationsPage/Jerusalem/Jerusalem.tsx
@@ -1,11 +1,16 @@
-import Header from "../Header/Header";
+import FullCity from "../FullCity/FullCity";
 import "./Jerusalem.css";
 
 function Jerusalem(): JSX.Element {
     return (
         <div className="Jerusalem">
-            <Header />
-			jerusalem
+            <FullCity city={"Jerusalem"} email='vasirental@jer.co.il'
+                address="Tora Mi'Tsiyon 8 st, Jerusalem." redirectPath="#" phoneNumber="" aboutLocation="The Jerusalem branch is
+                        placed in a easy to reach location between in Jerusalem. Close to Jerusalems old city, Makneh Yehuda Market,
+                        The Israel Museum, Israels Aquarium, Mount Herzl, Yad Va'Shem and of course the Knesset. makes it ideal
+                        for exoloring Jerusalem and Israel." reachOfficeInstructions="Our office is located at Tora Mi'Tsiyon 8 st,
+                        Jerusalem. You can find
+                        us right beside Jerusalems Central Station and the Allenby square. We are located on the 3rd floor."/>
         </div>
     );
 }
