9c041c8d2df9c322f840cfa45371839768bd2fc7
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-22 11:44:30 +0300

    added text to remaining services

--- src/Components/Layout/MainArea/ServicesPage/OneDay/OneDay.tsx
+++ src/Components/Layout/MainArea/ServicesPage/OneDay/OneDay.tsx
@@ -26,7 +26,6 @@ function OneDay(): JSX.Element {
                             <HashLink smooth to={appConfig.samePagePath + "whoisitfor"}><li>Who Is The Service For?</li></HashLink>
                             <HashLink smooth to={appConfig.samePagePath + "howitworks"}><li>How Does It Work?</li></HashLink>
                             <HashLink smooth to={appConfig.samePagePath + "taxesandwriteoff"}><li>Taxes & Business Write Off</li></HashLink>
-                            <HashLink smooth to={appConfig.samePagePath + "welcomebonus"}><li>Welcome Bonus!</li></HashLink>
                             <HashLink smooth to={appConfig.samePagePath + "contactus"}><li>Contact Us</li></HashLink>
                         </ul>
                     </div>
@@ -34,29 +33,36 @@ function OneDay(): JSX.Element {
                 <div className="content">
 
                     <h3 id="aboutservice">About The  Service</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
+                    <p>At Vasilenko Car Rental, we understand that not all journeys require an extended commitment. That's why
+                        we offer our convenient One-Day Car Rental service, designed to cater to your specific schedule. With
+                        options ranging from 12 hours, 18 hours, to a full 24 hours, you have the flexibility to choose the
+                        duration that suits your needs best. Whether you're a local resident looking to run errands, a traveler
+                        exploring a new city, or someone in need of a reliable vehicle for a full day, our one-day rental
+                        programs have you covered. Our diverse fleet of vehicles, competitive pricing, and straightforward
+                        booking process make it easy to secure the perfect car for a day, ensuring that you have the freedom to
+                        go where you need to, when you need to. Your short-term journey, no matter how busy or relaxed, begins
+                        with us.</p>
 
                     <h3 id="whoisitfor">Who Is The Service For?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
+                    <p>Our One-Day Rental service at Vasilenko Car Rental is tailored to meet the needs of individuals and
+                        travelers seeking short-term transportation solutions. It's ideal for local residents looking for a
+                        car to run errands, tourists exploring a new city, business professionals attending meetings, or
+                        anyone in need of a reliable vehicle for a single day. If you have a busy schedule or simply want
+                        the flexibility of a car for a day, our one-day rental programs offer the freedom to plan your day
+                        on your terms. Whether it's for business, leisure, or convenience, our one-day rentals are the perfect
+                        solution for those looking for a quick, hassle-free, and cost-effective way to get around.</p>
 
                     <h3 id="howitworks">How Does It Work?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
+                    <p></p>
 
                     <h3 id="taxesandwriteoff">Taxes & Business Write Off</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
-
-                    <h3 id="welcomebonus">Welcome Bonus!</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
+                    <p>At Vasilenko Car Rental, we're here to help you navigate the financial aspects of your car rental needs. One of
+                        the advantages of partnering with us is the potential for valuable tax deductions and business write-offs.
+                        Depending on your business structure and the nature of your rentals, you may be eligible to claim a portion
+                        of your car rental expenses as a tax deduction. By doing so, you can reduce your taxable income and ultimately
+                        lower your tax liability. It's a smart way to manage your finances and maximize savings while providing
+                        essential transportation for your business needs. Always consult with your accountant or tax advisor to
+                        understand how our car rental services can work to your advantage in your specific financial situation.</p>
 
                     <h3 id="contactus">Contact Us!</h3>
                     <p>form</p>
--- src/Components/Layout/MainArea/ServicesPage/OneWay/OneWay.tsx
+++ src/Components/Layout/MainArea/ServicesPage/OneWay/OneWay.tsx
@@ -25,7 +25,6 @@ function OneWay(): JSX.Element {
                             <HashLink smooth to={appConfig.samePagePath + "aboutservice"}><li>About The Service</li></HashLink>
                             <HashLink smooth to={appConfig.samePagePath + "whoisitfor"}><li>Who Is The Service For?</li></HashLink>
                             <HashLink smooth to={appConfig.samePagePath + "howitworks"}><li>How Does It Work?</li></HashLink>
-                            <HashLink smooth to={appConfig.samePagePath + "taxesandwriteoff"}><li>Taxes & Business Write Off</li></HashLink>
                             <HashLink smooth to={appConfig.samePagePath + "welcomebonus"}><li>Welcome Bonus!</li></HashLink>
                             <HashLink smooth to={appConfig.samePagePath + "contactus"}><li>Contact Us</li></HashLink>
                         </ul>
@@ -34,29 +33,36 @@ function OneWay(): JSX.Element {
                 <div className="content">
 
                     <h3 id="aboutservice">About The  Service</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
+                    <p>Vasilenko Car Rental offers an exceptional One-Way Car Rental service designed for travelers with
+                        unidirectional itineraries. Whether you're embarking on a road trip, relocating to a new city, or simply
+                        need a hassle-free way to get from point A to point B, our one-way rental service provides the perfect
+                        solution. Choose your preferred pickup location, and then drop off the vehicle at a different designated
+                        location, saving you the time and effort of returning the car to the original spot. This service is ideal
+                        for adventurers exploring new horizons, professionals seeking efficient travel options, and anyone looking for
+                        a seamless, one-way transportation experience. At Vasilenko Car Rental, we ensure that your journey, no matter
+                        the destination, is marked by convenience and flexibility.</p>
 
                     <h3 id="whoisitfor">Who Is The Service For?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
+                    <p>Our One-Way Car Rental service is perfect for a wide range of travelers and situations. It's ideal for
+                        individuals and families embarking on cross-country adventures, those moving to a new city, business
+                        professionals attending meetings or conferences, and anyone looking to simplify their travel experience.
+                        Whether you're exploring new horizons, relocating, or need a convenient transportation solution for a one-way
+                        trip, our service offers the flexibility to match your unique journey. We aim to make unidirectional travel
+                        stress-free and straightforward, allowing you to focus on your destination without the concern of returning
+                        the vehicle to its original location. With Vasilenko Car Rental, your one-way journey becomes an effortless
+                        and enjoyable experience.</p>
 
                     <h3 id="howitworks">How Does It Work?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
+                    <p></p>
 
                     <h3 id="taxesandwriteoff">Taxes & Business Write Off</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
-
-                    <h3 id="welcomebonus">Welcome Bonus!</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sequi tempore dolores laborum hic officia
-                        esse nemo id distinctio beatae incidunt earum aspernatur vel, facere labore rerum voluptatum voluptate
-                        vitae deleniti. Perspiciatis mollitia incidunt, expedita a cupiditate possimus numquam dolorem?</p>
+                    <p>At Vasilenko Car Rental, we're here to help you navigate the financial aspects of your car rental needs. One of
+                        the advantages of partnering with us is the potential for valuable tax deductions and business write-offs.
+                        Depending on your business structure and the nature of your rentals, you may be eligible to claim a portion
+                        of your car rental expenses as a tax deduction. By doing so, you can reduce your taxable income and ultimately
+                        lower your tax liability. It's a smart way to manage your finances and maximize savings while providing
+                        essential transportation for your business needs. Always consult with your accountant or tax advisor to
+                        understand how our car rental services can work to your advantage in your specific financial situation.</p>
 
                     <h3 id="contactus">Contact Us!</h3>
                     <p>form</p>
