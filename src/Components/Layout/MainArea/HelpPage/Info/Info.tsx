9992d5f74073da3e050f5a688f8a6ceba6a6c109
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-26 01:16:35 +0300

    added 4 more articles

--- src/Components/Layout/MainArea/HelpPage/Info/Info.tsx
+++ src/Components/Layout/MainArea/HelpPage/Info/Info.tsx
@@ -54,13 +54,26 @@ function Info(): JSX.Element {
                     <InfoItem header="Beyond Tel Aviv: road-tripping Israel's north" content="he joy of driving Israel’s north
                     coast is all about the detours. It takes just a couple of hours to zoom directly between..." imageName="8.png"
                         redirectPath="https://www.lonelyplanet.com/articles/beyond-tel-aviv-road-tripping-israels-north-coast" />
-
                 </div>
             </div>
             <div className="row-container">
                 <h2>Driving Laws and Regulations</h2>
                 <div className="row-content">
+                    <InfoItem header="Driving in Israel - For Foreigners" content="Israel is a small country, making it easily navigable by car.
+                    The last decade has seen great infrastructure improvements..." imageName="9.png"
+                        redirectPath="https://www.tripsavvy.com/driving-in-israel-5090148" />
 
+                    <InfoItem header="FAQ – Israeli Traffic Law & Regulation" content="o you plan on renting a car in Israel to explore this
+                    wonderful country? We have done the same and cannot wait..." imageName="10.png"
+                        redirectPath="https://lawoffice.org.il/en/faq-israeli-traffic-law/" />
+
+                    <InfoItem header="Driving in Israel: Everything To Know" content="This article, FAQ – Israeli Traffic Law, addresses many
+                    issues relevant to anyone that has been involved in a traffic..." imageName="11.png"
+                        redirectPath="https://www.laidbacktrip.com/posts/driving-in-israel" />
+
+                    <InfoItem header="Traffic Laws in Israel - For Travelers" content="Statistics say that it is safer to be in a vehicle in Israel than
+                    anywhere in North America. There are fewer fatalities in..." imageName="12.png"
+                        redirectPath="https://commonlegalquestions.com/traffic-laws-in-israel/" />
                 </div>
             </div>
             <div className="row-container">
