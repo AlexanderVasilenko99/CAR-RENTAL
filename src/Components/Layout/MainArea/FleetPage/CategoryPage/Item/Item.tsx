cd318536c0324dfa7498f5040a53e086efbea316
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-28 12:46:05 +0300

    ^^

--- src/Components/Layout/MainArea/FleetPage/CategoryPage/Item/Item.tsx
+++ src/Components/Layout/MainArea/FleetPage/CategoryPage/Item/Item.tsx
@@ -7,47 +7,47 @@ function FleetCategoryItem(props: FleetCategoryItemModel): JSX.Element {
     return (
         <div className="Item">
             <div className="imgHolder">
-                <img src={require(`../../../../../../Assets/Images/Vehicles/${props.imageName}`)} />
+                <img src={require(`../../../../../../Assets/Images/Fleet/Vehicles/${props.imageName}`)} />
             </div>
             <h1>{props.vehicleName}</h1>
             <div className="stats">
                 <div className="initialStats">
                     <div>
-                        <img src={require(`../../../../../../Assets/Images/transmissionicon.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/transmissionicon.png`)} />
                         {props.transmission}
                     </div>
                     <div>
-                        <img src={require(`../../../../../../Assets/Images/personicon.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/personicon.png`)} />
                         {props.passengers}
                     </div>
                     <div>
-                        <img src={require(`../../../../../../Assets/Images/cardooricon.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/cardooricon.png`)} />
                         {props.doors}
                     </div>
                     <div>
-                        <img src={require(`../../../../../../Assets/Images/suitcaseicon.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/suitcaseicon.png`)} />
                         {props.luggage}
                     </div>
                 </div>
                 <div className="moreStats">
                     <div>
-                        <img src={require(`../../../../../../Assets/Images/trunkicon.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/trunkicon.png`)} />
                         Storage: {props.trunkCapacity}
                     </div>
                     <div>
-                        <img src={require(`../../../../../../Assets/Images/fuelpumpicon.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/fuelpumpicon.png`)} />
                         Fuel: {props.fuel}
                     </div>
                     <div>
-                        <img src={require(`../../../../../../Assets/Images/gascanicon.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/gascanicon.png`)} />
                         Tank Capacity: {props.tankCapacity}
                     </div>
                     {props.radio && <div>
-                        <img src={require(`../../../../../../Assets/Images/radioicon.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/radioicon.png`)} />
                         Radio
                     </div>}
                     {props.airConditioning && <div>
-                        <img src={require(`../../../../../../Assets/Images/snowflake.png`)} />
+                        <img src={require(`../../../../../../Assets/Images/Fleet/Icons/snowflake.png`)} />
                         Air Conditioning
                     </div>}
                 </div>
