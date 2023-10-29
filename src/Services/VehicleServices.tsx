a29b3975f6187965e9cf72c1e8d10cb93b0f05ab
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-29 21:16:53 +0200

    changed axios get request path to accomodate github pages

--- src/Services/VehicleServices.tsx
+++ src/Services/VehicleServices.tsx
@@ -3,10 +3,11 @@ import AllVehiclesByCategories from "../Models/AllVehiclesByCategories";
 
 class VehicleServices {
     public async GetAllVehicles(): Promise<AllVehiclesByCategories> {
-        const response = await axios.get("/Vehicles.json");
+        // const response = await axios.get("/Vehicles.json");
+        const response = await axios.get("https://raw.githubusercontent.com/AlexanderVasilenko99/car-rental/main/public/Vehicles.json");
 
         const data: AllVehiclesByCategories = response.data.vehicles;
-        
+
         // const action = { type: VehicleActionTypes.GetAllVehicles, payload: vehicles }
         // vehiclesState.dispatch(action);
 
