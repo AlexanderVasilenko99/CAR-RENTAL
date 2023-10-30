cde04fd2652b4182bd07f43165d29210fa32e2f1
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-31 01:26:23 +0200

    added map to lawsregulations

--- src/Components/Layout/MainArea/HelpPage/Info/LawsRegulations/LawsRegulations.tsx
+++ src/Components/Layout/MainArea/HelpPage/Info/LawsRegulations/LawsRegulations.tsx
@@ -1,27 +1,29 @@
+import { InfoItemModel } from "../../../../../../Models/InfoItemModel";
 import InfoItem from "../InfoItem/InfoItem";
 import "./LawsRegulations.css";
 
 function LawsRegulations(): JSX.Element {
+    const articles: InfoItemModel[] = [
+        new InfoItemModel("Driving in Israel - For Foreigners", "9.png",
+            "Israel is a small country, making it easily navigable by car. The last decade has seen great infrastructure improvements...",
+            "https://www.tripsavvy.com/driving-in-israel-5090148"),
+        new InfoItemModel("FAQ – Israeli Traffic Law & Regulation", "10.png",
+            "So you plan on renting a car in Israel to explore this wonderful country? We have done the same and cannot wait...",
+            "https://lawoffice.org.il/en/faq-israeli-traffic-law/"),
+        new InfoItemModel("Driving in Israel: Everything To Know", "11.png",
+            "This article, FAQ – Israeli Traffic Law, addresses many issues relevant to anyone that has been involved in a traffic...",
+            "https://www.laidbacktrip.com/posts/driving-in-israel"),
+        new InfoItemModel("Traffic Laws in Israel - For Travelers", "12.png",
+            "Statistics say that it is safer to be in a vehicle in Israel than anywhere in North America. There are fewer fatalities in...",
+            "https://commonlegalquestions.com/traffic-laws-in-israel/")
+    ]
     return (
         <div>
             <h2>Driving Laws and Regulations</h2>
             <div className="LawsRegulations">
                 <div className="row-content">
-                    <InfoItem header="Driving in Israel - For Foreigners" content="Israel is a small country, making it easily navigable by car.
-                    The last decade has seen great infrastructure improvements..." imageName="9.png"
-                        redirectPath="https://www.tripsavvy.com/driving-in-israel-5090148" />
-
-                    <InfoItem header="FAQ – Israeli Traffic Law & Regulation" content="o you plan on renting a car in Israel to explore this
-                    wonderful country? We have done the same and cannot wait..." imageName="10.png"
-                        redirectPath="https://lawoffice.org.il/en/faq-israeli-traffic-law/" />
-
-                    <InfoItem header="Driving in Israel: Everything To Know" content="This article, FAQ – Israeli Traffic Law, addresses many
-                    issues relevant to anyone that has been involved in a traffic..." imageName="11.png"
-                        redirectPath="https://www.laidbacktrip.com/posts/driving-in-israel" />
-
-                    <InfoItem header="Traffic Laws in Israel - For Travelers" content="Statistics say that it is safer to be in a vehicle in Israel than
-                    anywhere in North America. There are fewer fatalities in..." imageName="12.png"
-                        redirectPath="https://commonlegalquestions.com/traffic-laws-in-israel/" />
+                    {articles.map(a => <InfoItem header={a.header} content={a.content}
+                        imageName={a.imageName} redirectPath={a.redirectPath} />)}
                 </div>
             </div>
         </div>
