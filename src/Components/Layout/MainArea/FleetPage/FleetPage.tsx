0c6071f7f7d4fc95cee512058b7d3b34d032f8fb
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-30 14:00:36 +0200

    added map to fleet page

--- src/Components/Layout/MainArea/FleetPage/FleetPage.tsx
+++ src/Components/Layout/MainArea/FleetPage/FleetPage.tsx
@@ -2,8 +2,18 @@ import { NavLink } from "react-router-dom";
 import "./FleetPage.css";
 import FleetItem from "./FleetItem/FleetItem";
 import appConfig from "../../../../Utils/AppConfig";
+import VehicleCategoryItem from "../../../../Models/VehicleCategoryItem";
 
 function FleetPage(): JSX.Element {
+    const fleetItems: VehicleCategoryItem[] = [
+        new VehicleCategoryItem("Suv & Off Road", "jeep.png", appConfig.fleetPageSuvOffRoadPath),
+        new VehicleCategoryItem("Luxury", "porsche.png", appConfig.fleetPageLuxuryPath),
+        new VehicleCategoryItem("Small", "micra.png", appConfig.fleetPageSmallPath),
+        new VehicleCategoryItem("Medium", "focus.png", appConfig.fleetPageMediumPath),
+        new VehicleCategoryItem("Large", "sentra.png", appConfig.fleetPageLargePath),
+        new VehicleCategoryItem("Vans & Trucks", "mercedes.png", appConfig.fleetPageVansTrucksPath),
+        new VehicleCategoryItem("Motorcycles & Scooters", "sv650.png", appConfig.fleetPageMotorcyclesScootersPath),
+    ]
     return (
         <div className="FleetPage">
             <h1>Our fleet offers a variety of over 150 vehicle models across all categories for you to choose from!&nbsp;
@@ -11,15 +21,8 @@ function FleetPage(): JSX.Element {
                     View All
                 </NavLink>
             </h1>
-
-            <FleetItem vehicleCategoryName="Suv & Off Road" vehicleCategoryImgName="jeep.png" vehicleCategoryRedirectPath={appConfig.fleetPageSuvOffRoadPath} />
-            <FleetItem vehicleCategoryName="Luxury" vehicleCategoryImgName="porsche.png" vehicleCategoryRedirectPath={appConfig.fleetPageLuxuryPath} />
-            <FleetItem vehicleCategoryName="Small" vehicleCategoryImgName="micra.png" vehicleCategoryRedirectPath={appConfig.fleetPageSmallPath} />
-            <FleetItem vehicleCategoryName="Medium" vehicleCategoryImgName="focus.png" vehicleCategoryRedirectPath={appConfig.fleetPageMediumPath} />
-            <FleetItem vehicleCategoryName="Large" vehicleCategoryImgName="sentra.png" vehicleCategoryRedirectPath={appConfig.fleetPageLargePath} />
-            <FleetItem vehicleCategoryName="Vans & Trucks" vehicleCategoryImgName="mercedes.png" vehicleCategoryRedirectPath={appConfig.fleetPageVansTrucksPath} />
-            <FleetItem vehicleCategoryName="Motorcycles & Scooters" vehicleCategoryImgName="sv650.png" vehicleCategoryRedirectPath={appConfig.fleetPageMotorcyclesScootersPath} />
-
+            {fleetItems.map(i => <FleetItem vehicleCategoryName={i.vehicleCategoryName}
+                vehicleCategoryImgName={i.vehicleCategoryImgName} vehicleCategoryRedirectPath={i.vehicleCategoryRedirectPath} />)}
             <NavLink to={appConfig.fleetPageAllVehiclesPath}>
                 <h3>Or browse all vehicles here</h3>
             </NavLink>
--- src/Models/VehicleCategoryItem.tsx
+++ src/Models/VehicleCategoryItem.tsx
@@ -2,5 +2,12 @@ class VehicleCategoryItem {
     public vehicleCategoryName: string;
     public vehicleCategoryImgName: string;
     public vehicleCategoryRedirectPath: string;
+
+    constructor(vehicleCategoryName: string, vehicleCategoryImgName: string, vehicleCategoryRedirectPath: string) {
+        this.vehicleCategoryName = vehicleCategoryName;
+        this.vehicleCategoryImgName = vehicleCategoryImgName;
+        this.vehicleCategoryRedirectPath = vehicleCategoryRedirectPath;
+
+    }
 }
 export default VehicleCategoryItem;
