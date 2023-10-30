aa08d23cd28896c0377ee66684cf11bc3438da47
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-10-31 00:33:33 +0200

    added map to services

--- src/Components/Layout/MainArea/ServicesPage/ServicesPage.tsx
+++ src/Components/Layout/MainArea/ServicesPage/ServicesPage.tsx
@@ -1,43 +1,32 @@
+import { SingleServiceModel } from "../../../../Models/SingleServiceModel";
 import appConfig from "../../../../Utils/AppConfig";
 import "./ServicesPage.css";
 import SingleService from "./SingleService/SingleService";
 
 function ServicesPage(): JSX.Element {
+
+    const services: SingleServiceModel[] = [
+        new SingleServiceModel("Business Rental", "handshake2.png", appConfig.servicesBusinessRentalPagePath,
+            "Discover a unique program built exclusively for businesses of all scales."),
+        new SingleServiceModel("Monthly Rental", "calendar.png", appConfig.servicesMonthlyRentalPagePath,
+            "Looking to rent for a while? We got you covered!"),
+        new SingleServiceModel("Chauffeur Services", "chauffeur.png", appConfig.servicesChauffeurPagePath,
+            "We offer a variety of chauffer services at any time all year around."),
+        new SingleServiceModel("Group Car Rental", "group.png", appConfig.servicesGroupRentalPagePath,
+            "Discover the best prices and discounts for groups renting 3 cars or more!"),
+        new SingleServiceModel("One Day Car Rental", "oneday.png", appConfig.servicesOneDayRentalPagePath,
+            "Learn about our affordable one day rental program."),
+        new SingleServiceModel("One Way Car Rental", "destination.png", appConfig.servicesOneWayRentalPagePath,
+            "Learn about our affordable one way rental program."),
+    ]
+
     return (
         <div className="ServicesPage">
             <div className="image-container">
                 {/* <img src={require("../../../../Assets/Images/Services/road2.png")} /> */}
-                <div>
-                    <SingleService serviceHeader="Business Rental" serviceImageName="handshake2.png"
-                        serviceParagraph="Discover a unique program built exclusively for businesses of all scales."
-                        destinationUrl={appConfig.servicesBusinessRentalPagePath} />
-                </div>
-                <div>
-                    <SingleService serviceHeader="Monthly Rental" serviceImageName="calendar.png"
-                        serviceParagraph="Looking to rent for a while? We got you covered!"
-                        destinationUrl={appConfig.servicesMonthlyRentalPagePath} />
-                </div>
-                <div>
-                    <SingleService serviceHeader="Chauffeur Services" serviceImageName="chauffeur.png"
-                        serviceParagraph="We offer a variety of chauffer services at any time all year around."
-                        destinationUrl={appConfig.servicesChauffeurPagePath} />
-                </div>
-                <div>
-                    <SingleService serviceHeader="Group Car Rental" serviceImageName="group.png"
-                        serviceParagraph="Discover the best prices and discounts for groups renting 3 cars or more!"
-                        destinationUrl={appConfig.servicesGroupRentalPagePath} />
-                </div>
-                <div>
-                    <SingleService serviceHeader="One Day Car Rental" serviceImageName="oneday.png"
-                        serviceParagraph="Learn about our affordable one day rental program."
-                        destinationUrl={appConfig.servicesOneDayRentalPagePath} />
-                </div>
-                <div>
-                    <SingleService serviceHeader="One Way Car Rental" serviceImageName="destination.png"
-                        serviceParagraph="Learn about our affordable one way rental program."
-                        destinationUrl={appConfig.servicesOneWayRentalPagePath} />
-                </div>
-
+                {services?.map(s => <div><SingleService serviceHeader={s.serviceHeader} serviceImageName={s.serviceImageName}
+                    serviceParagraph={s.serviceParagraph}
+                    destinationUrl={s.destinationUrl} /></div>)}
             </div>
 
         </div>
--- src/Models/SingleServiceModel.tsx
+++ src/Models/SingleServiceModel.tsx
@@ -1,6 +1,13 @@
 export class SingleServiceModel {
     public serviceHeader: string;
-    public serviceParagraph: string;
     public serviceImageName: string;
     public destinationUrl: string;
+    public serviceParagraph: string;
+
+    constructor(serviceHeader: string, serviceImageName: string, destinationUrl: string, serviceParagraph: string) {
+        this.serviceHeader = serviceHeader;
+        this.serviceImageName = serviceImageName;
+        this.destinationUrl = destinationUrl;
+        this.serviceParagraph = serviceParagraph;
+    }
 }
