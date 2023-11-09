9b05805526d248680827804176bd6b16b64c8a9a
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-11-09 16:13:31 +0200

    added cv to downloadable files

--- src/Components/Layout/MainArea/AboutUs/AboutUs.tsx
+++ src/Components/Layout/MainArea/AboutUs/AboutUs.tsx
@@ -1,9 +1,10 @@
 import "./AboutUs.css";
-
+import cvFile from "../../../../Assets/Files/Downloads/Alexander-Vasilenko-CV.pdf"
 function AboutUs(): JSX.Element {
     return (
         <div className="AboutUs">
-			Alex Vas
+            Alex Vas
+            <a href="This is my CV" download={cvFile}>CV</a>
         </div>
     );
 }
