8ab956a6627c59bda891dc8e84cd6225e9618b3c
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-11-11 14:01:14 +0200

    added why us section

--- src/Components/Layout/MainArea/HomePage/WhyUs/WhyUs.tsx
+++ src/Components/Layout/MainArea/HomePage/WhyUs/WhyUs.tsx
@@ -1,17 +1,10 @@
-import Perk, { PerkModel } from "./Perk/Perk";
 import "./WhyUs.css";
-
 function WhyUs(): JSX.Element {
-    const perks: PerkModel[] = [
-        new PerkModel("IT'S EASY!", "easy.png"),
-        new PerkModel("IT'S AFFORDABLE!", "easy.png")
-    ]
     return (
         <div className="WhyUs">
             <div className="WhyUsContent">
-                {perks.map(p =>
-                    <Perk header={p.header} imageName={p.imageName} />
-                )}
+                <img src={require('../../../../../Assets/Images/HomePage/2.png')} alt="" />
+                <img src={require('../../../../../Assets/Images/HomePage/3.png')} alt="" />
             </div>
         </div>
     );
