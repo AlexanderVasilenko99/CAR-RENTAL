fd9be82bdffde662acd06addad207ba3620d093b
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-11-26 19:45:12 +0200

    edited about me

--- src/Components/Layout/MainArea/AboutUs/AboutUs.tsx
+++ src/Components/Layout/MainArea/AboutUs/AboutUs.tsx
@@ -3,7 +3,14 @@ import cvFile from "../../../../Assets/Files/Downloads/Alexander-Vasilenko-CV.pd
 function AboutUs(): JSX.Element {
     return (
         <div className="AboutUs">
-            Alex Vasilenko
+            <div>About Me & This Project</div>
+            <div>picture</div>
+            <div>Hi There! I'm</div>
+            <div>Alexander JR Vasilenko</div>
+            <div>Full Stack Developer</div>
+            <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, error harum. Provident sit praesentium, vero quidem
+                eum autem explicabo, quos ipsam aut expedita iure harum aliquid illum adipisci itaque? Neque tempora mollitia animi
+                quaerat similique debitis cumque nihil ipsum asperiores!</div>
             <a href="This is my CV" download={cvFile}>CV</a>
         </div>
     );
