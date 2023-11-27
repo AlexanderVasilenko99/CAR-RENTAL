d0e6f4d7dea8863c537cd63a7d1f7796dd4b2d96
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-11-27 11:13:28 +0200

    added a bit o css

--- src/Components/Layout/MainArea/AboutUs/AboutUs.tsx
+++ src/Components/Layout/MainArea/AboutUs/AboutUs.tsx
@@ -3,14 +3,14 @@ import cvFile from "../../../../Assets/Files/Downloads/Alexander-Vasilenko-CV.pd
 function AboutUs(): JSX.Element {
     return (
         <div className="AboutUs">
-            <div>About Me & This Project</div>
+            <h1>About Me & This Project</h1>
             <div>picture</div>
-            <div>Hi There! I'm</div>
-            <div>Alexander JR Vasilenko</div>
-            <div>Full Stack Developer</div>
-            <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, error harum. Provident sit praesentium, vero quidem
+            <h2>👋🏻Hi There! I'm</h2>
+            <h3>Alexander JR Vasilenko</h3>
+            <h4>Full Stack Developer</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, error harum. Provident sit praesentium, vero quidem
                 eum autem explicabo, quos ipsam aut expedita iure harum aliquid illum adipisci itaque? Neque tempora mollitia animi
-                quaerat similique debitis cumque nihil ipsum asperiores!</div>
+                quaerat similique debitis cumque nihil ipsum asperiores!</p>
             <a href="This is my CV" download={cvFile}>CV</a>
         </div>
     );
